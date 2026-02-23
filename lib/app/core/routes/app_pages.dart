b00d03d80fa9d16264e0c6d562e7e5ae8296814b
samiriihglobal <samir.iihglobal@gmail.com>
2026-02-23 12:26:51 +0530

    Add splash and one-time onboarding flow

--- lib/app/core/routes/app_pages.dart
+++ lib/app/core/routes/app_pages.dart
@@ -3,13 +3,23 @@ import 'package:firebase_auth_demo/app/modules/auth/login/login_page.dart';
 import 'package:firebase_auth_demo/app/modules/auth/signup/signup_page.dart';
 import 'package:firebase_auth_demo/app/modules/home/home_binding.dart';
 import 'package:firebase_auth_demo/app/modules/home/home_page.dart';
+import 'package:firebase_auth_demo/app/modules/onboarding/onboarding_page.dart';
+import 'package:firebase_auth_demo/app/modules/splash/splash_page.dart';
 import 'package:get/get.dart';
 import 'app_routes.dart';
 
 class AppPages {
-  static const INITIAL = AppRoutes.LOGIN;
+  static const INITIAL = AppRoutes.SPLASH;
 
   static final routes = [
+    GetPage(
+      name: AppRoutes.SPLASH,
+      page: () => const SplashPage(),
+    ),
+    GetPage(
+      name: AppRoutes.ONBOARDING,
+      page: () => const OnboardingPage(),
+    ),
     GetPage(
       name: AppRoutes.LOGIN,
       page: () => LoginPage(),
